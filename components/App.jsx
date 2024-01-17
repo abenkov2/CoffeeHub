e9452d29fcf76424ac8f1dce92e0d9165e978922
abenkov2 <akbenkov79@gmail.com>
2024-01-17 15:46:27 -0500

    front end skeleton rendering

---
+++ components/App.jsx
@@ -0,0 +1,22 @@
+/**
+ * ************************************
+ *
+ * @module  App.jsx
+ * @author
+ * @date
+ * @description
+ *
+ * ************************************
+ */
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Coffee App</h1>
+    </div>
+  );
+};
+
+export default App;
--- server/server.js
+++ server/server.js
@@ -11,3 +11,24 @@ if (process.env.NODE_ENV === 'production') {
     return res.status(200).sendFile(path.join(__dirname, '../index.html'));
   });
 }
+
+app.use((req, res) =>
+  res.status(404).sendFile(path.resolve(__dirname, './client/404.html'))
+);
+
+// Global error handling middleware
+// How can we trigger this to run?
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}...`);
+});
